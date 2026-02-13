94b9fbbe1e1cf2dacba5ffb01359c7aeded267c3
Favoriot IoT Platform <33713554+Favoriot@users.noreply.github.com>
2026-02-13 16:28:29 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,23 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="favoriotml",
-    version="1.0.0",
-    author="mnazrinnapiah",
-    description="Edge ML SDK for Favoriot local inference",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/favoriot/favoriot-ml-sdk",
+    version="0.1.0",
+    description="Favoriot ML Inference SDK",
     packages=find_packages(),
+    python_requires=">=3.12",
     install_requires=[
         "numpy",
-        "scikit-learn",
         "joblib",
+        "scikit-learn",
         "onnxruntime",
+        "paho-mqtt"
     ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.12',
 )
